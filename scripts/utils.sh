eef508d38a897022521c808d09c40390055f5c64
Nelson Estevão <nelsonmestevao@proton.me>
2024-03-25 07:30:27 +0000

    Release 0.15.0

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -98,4 +98,4 @@ function timestamp() {
   date --utc '+%Y%m%d%H%M%S'
 }
 
-([ "$0" = "${BASH_SOURCE[0]}" ] && display_version 0.14.0) || true
+([ "$0" = "${BASH_SOURCE[0]}" ] && display_version 0.15.0) || true
